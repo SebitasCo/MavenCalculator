1964f49ec1f2d48fe45b0037224258372a5dc994
SENA <sebastianmg1700@gmail.com>
2024-10-22 09:40:54 -0500

    final

---
+++ src/main/java/com/calculator/otg/Calculadora.java
@@ -0,0 +1,13 @@
+package com.calculator.otg;
+
+
+public class Calculadora {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
